6acea5e376f99f6f50b50e7511866c4f48e3fbb7
Chen Guo <chen@factual.com>
2014-11-23 05:43:54 -0800

    Project.clj: 0.0.1-SNAPSHOT

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject hanabi "0.0.1-SNAPSHOT"
+  :description "Hanabi game player"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :java-source-paths ["src/java"])
